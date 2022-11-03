0900826ae7eec792a3b42be74fbbcc96c65552f8
Ronny Velardez <ronnyjmv@gmail.com>
2022-11-02 22:55:00 -0300

    feat: implement create table pools

---
+++ server/prisma/migrations/20221101040149_create_table_pools/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "pool" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "code" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "pool_code_key" ON "pool"("code");
