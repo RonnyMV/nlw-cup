be8d8377837cee9e8af910ade02c20ea67945fb5
Ronny Velardez <ronnyjmv@gmail.com>
2022-11-02 22:55:11 -0300

    feat: implement create db structure

---
+++ server/prisma/migrations/20221102212007_create_db_structure/migration.sql
@@ -0,0 +1,60 @@
+-- CreateTable
+CREATE TABLE "Participant" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "userId" TEXT NOT NULL,
+    "poolId" TEXT NOT NULL,
+    CONSTRAINT "Participant_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "Participant_poolId_fkey" FOREIGN KEY ("poolId") REFERENCES "pool" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "avatarUrl" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- CreateTable
+CREATE TABLE "Game" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "date" DATETIME NOT NULL,
+    "firstTeamCountryCode" TEXT NOT NULL,
+    "secondTeamCountryCode" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Guess" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "firstTeamPoints" INTEGER NOT NULL,
+    "secondTeamPoints" INTEGER NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "gameId" TEXT NOT NULL,
+    "participantId" TEXT NOT NULL,
+    CONSTRAINT "Guess_participantId_fkey" FOREIGN KEY ("participantId") REFERENCES "Participant" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "Guess_gameId_fkey" FOREIGN KEY ("gameId") REFERENCES "Game" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_pool" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "code" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "ownerId" TEXT,
+    CONSTRAINT "pool_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "User" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+);
+INSERT INTO "new_pool" ("code", "createdAt", "id", "title") SELECT "code", "createdAt", "id", "title" FROM "pool";
+DROP TABLE "pool";
+ALTER TABLE "new_pool" RENAME TO "pool";
+CREATE UNIQUE INDEX "pool_code_key" ON "pool"("code");
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Participant_userId_poolId_key" ON "Participant"("userId", "poolId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
