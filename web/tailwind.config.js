d757a43be2294e33a0eb846a66df85c412c666b0
Ronny Velardez <ronnyjmv@gmail.com>
2022-11-06 16:33:58 -0300

    feat: config tailwind cont path

---
+++ web/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.tsx',
+  ],
+  theme: {
+    extend: {
+      // fontFamily: {
+      //   sans: "Roboto, san-serif"
+      // },
+      // colors: {
+      //   gray: {
+      //     900: '#121214'
+      //   }
+      // }
+    },
+  },
+  plugins: [],
+}
